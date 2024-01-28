5467768990437075f730087fc6f94a1fa4b9f255
Bukunmi Shodipo <bukunmi@mit.edu>
2024-01-28 14:39:29 -0500

    flare

---
+++ client/src/components/modules/EditProfileButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EditProfileButton = (props) => {
+  const displayEditOptions = () => {};
+  
+  return (
+    <>
+      <button onClick={displayEditOptions}>Edit</button>
+    </>
+  );
+};
+
+export default EditProfileButton;
